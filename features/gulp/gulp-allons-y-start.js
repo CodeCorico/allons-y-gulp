d993f5fb3addb108c281ef5c2c8779238ecdf1fb
Xavier Boubert <xavier@boubert.fr>
2016-10-23 15:04:30 +0200

    fix(gulp allons-y start): remove "node" execPath encapsulation

--- features/gulp/gulp-allons-y-start.js
+++ features/gulp/gulp-allons-y-start.js
@@ -4,6 +4,6 @@ module.exports = {
   name: 'Allons-y Gulp',
   enabled: !process.env.GULP || process.env.GULP == 'true' || false,
   spawn: true,
-  spawnCommands: ['"' + process.execPath + '"', './node_modules/gulp/bin/gulp'],
+  spawnCommands: [process.execPath, './node_modules/gulp/bin/gulp'],
   spawnMaxRestarts: process.env.GULP_WATCHER && process.env.GULP_WATCHER == 'true' ? 10 : 1
 };
