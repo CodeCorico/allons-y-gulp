cdcd2de2c96982ecb1d134d72245f1c4b31609b4
Xavier Boubert <xavier@boubert.fr>
2016-11-24 11:25:35 +0100

    fix(gulp allons-y start): set the restart to 1
    The 0 value on forever-monitor means to restart it infinitely.

--- features/gulp/gulp-allons-y-start.js
+++ features/gulp/gulp-allons-y-start.js
@@ -5,5 +5,5 @@ module.exports = {
   enabled: !process.env.GULP || process.env.GULP == 'true' || false,
   spawn: true,
   spawnCommands: [process.execPath, './node_modules/gulp/bin/gulp'],
-  spawnMaxRestarts: process.env.GULP_WATCHER && process.env.GULP_WATCHER == 'true' ? 10 : 0
+  spawnMaxRestarts: process.env.GULP_WATCHER && process.env.GULP_WATCHER == 'true' ? 10 : 1
 };
